b9c80fa859bed8fa49bef445e437d1d81216b51d
PC <PC@DESKTOP-NUP7HH3>
2024-11-25 20:12:53 +0100

    Dodaj pliki projektów.

---
+++ Personal Organizer/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PersonalOrganizer.Models;
+
+namespace PersonalOrganizer.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<EventModel> Events { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=C:\\Users\\PC\\source\\repos\\Personal Organizer\\Personal Organizer\\personal_organizer.db");
+        }
+
+    }
+}
---
+++ Personal Organizer/ViewModels/RelayCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace PersonalOrganizer.ViewModels
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        // Konstruktor RelayCommand
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute)); // Zapewnienie, że _execute nie jest null
+            _canExecute = canExecute; // Pozwalamy na null dla canExecute
+        }
+
+        // Metoda sprawdzająca, czy polecenie może zostać wykonane
+        public bool CanExecute(object parameter)
+        {
+            // Jeśli _canExecute jest null, traktujemy jako zawsze wykonane
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        // Metoda wykonująca polecenie
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        // Podnosi zdarzenie, które informuje o zmianie w wyniku CanExecute
+        public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
